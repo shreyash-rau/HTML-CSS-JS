609c8f747b9ff878dd27c06b32cdb1b49f764754
Shreyash_Raut <67021340+shreyash-rau@users.noreply.github.com>
2023-09-07 18:48:23 +0530

    Add files via upload
    Using HTML, CSS, and JavaScript I made a Login Page.

---
+++ main3.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('submit');
+button.addEventListener('click', async event =>{
+
+var x = document.getElementById("login");
+if (button.innerHTML) {
+    x.innerHTML = " <br> Login Successfully <br><br>&nbsp;<a class=goback href=#>GO Back";
+    } else {
+        x.innerHTML;
+    }
+});
